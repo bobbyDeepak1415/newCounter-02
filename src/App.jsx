c5b87a4ce97e4484bd05061c755cd55206a5bb36
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-16 19:04:34 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { decrement, increment } from "./Counter/counterSlice2";
 
 function App() {
-  const dispatch = useDispatch();
-
   return (
     <>
-      <h2>
-        Your counter is at{" "}
-        <span>{useSelector((state) => state.counter.value)}</span>
-      </h2>
-      <button onClick={() => dispatch(decrement())}>-</button>
-      <button onClick={() => dispatch(increment())}>+</button>
+      <h2>Your counter is at </h2>
     </>
   );
 }
