a9013ea7a869ab3e901f60072577749c58db4732
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-16 18:41:43 +0530

    setup done

--- src/Counter/CounterSlice.jsx
+++ src/Counter/CounterSlice.jsx
@@ -1,26 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit'
-import React from 'react'
+import { createSlice } from "@reduxjs/toolkit";
+import React from "react";
 
-const initialState={
-
-}
-
+const initialState = {};
 
 const CounterSlice = createSlice({
-    name:"counter",
-    initialState,
-    reducers:{
-        increment:(state)=>{
-state.value+=1
-        },
-        decrement:(state)=>{
-            state.value>0?state.value-=1
-        }
-        
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      if (state.value >= 0) 
+        state.value -= 1;
+      
+    },
+  },
+});
 
-    }
-}) 
-
-
-
-export default CounterSlice.reducer
+export default CounterSlice.reducer;
